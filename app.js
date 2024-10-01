3c557941938fb2a4efe2bcceaf01923d7e3c81c6
Manon <manon@MacBook-Pro-de-Manon-7.local>
2024-10-01 16:22:43 +0200

    coucou

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
+//variables pour websocket
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
 app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + 'public/index.html');
+})
 
 app.post("/api/gitlab", (req, res) => {
   console.log(req.body);
@@ -12,3 +20,4 @@ app.post("/api/gitlab", (req, res) => {
 app.listen(3000, () => {
   console.log("listenning on 3000");
 });
+
